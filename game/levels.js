211f96a252b4fbd1e6913b49c53e887c2935798e
Derick Antony D <derickantony51112@gmail.com>
2026-09-10 20:15:09 +0530

    Fix levels 6-10 layouts and progression

--- game/levels.js
+++ game/levels.js
@@ -60,74 +60,81 @@ const LEVELS = [
     {
         platforms: [
             {x: 0, y: 430, width: 180, height: 30},
-            {x: 250, y: 330, width: 120, height: 30},
-            {x: 450, y: 400, width: 110, height: 30},
-            {x: 650, y: 270, width: 120, height: 30},
-            {x: 850, y: 350, width: 100, height: 30},
-            {x: 1050, y: 220, width: 120, height: 30},
-            {x: 1280, y: 320, width: 180, height: 30}
-        ]
+            {x: 230, y: 380, width: 130, height: 30},
+            {x: 410, y: 330, width: 130, height: 30},
+            {x: 590, y: 380, width: 130, height: 30},
+            {x: 770, y: 310, width: 130, height: 30},
+            {x: 950, y: 360, width: 130, height: 30},
+            {x: 1130, y: 300, width: 180, height: 30}
+        ],
+
+        goal: {
+            x: 1220,
+            y: 220,
+            width: 70,
+            height: 80
+        }
     },
 
     // LEVEL 7
     {
         platforms: [
             {x: 0, y: 430, width: 170, height: 30},
-            {x: 240, y: 360, width: 110, height: 30},
-            {x: 430, y: 280, width: 100, height: 30},
-            {x: 610, y: 380, width: 100, height: 30},
-            {x: 800, y: 250, width: 100, height: 30},
-            {x: 1000, y: 340, width: 100, height: 30},
-            {x: 1200, y: 230, width: 120, height: 30},
-            {x: 1450, y: 320, width: 180, height: 30}
+            {x: 220, y: 380, width: 120, height: 30},
+            {x: 390, y: 320, width: 120, height: 30},
+            {x: 560, y: 370, width: 120, height: 30},
+            {x: 730, y: 300, width: 120, height: 30},
+            {x: 900, y: 350, width: 120, height: 30},
+            {x: 1070, y: 280, width: 120, height: 30},
+            {x: 1240, y: 330, width: 180, height: 30}
         ]
     },
 
     // LEVEL 8
     {
         platforms: [
-            {x: 0, y: 430, width: 160, height: 30},
-            {x: 230, y: 350, width: 100, height: 30},
-            {x: 410, y: 240, width: 90, height: 30},
-            {x: 590, y: 330, width: 90, height: 30},
-            {x: 770, y: 210, width: 90, height: 30},
-            {x: 950, y: 300, width: 90, height: 30},
-            {x: 1130, y: 200, width: 90, height: 30},
-            {x: 1320, y: 290, width: 100, height: 30},
-            {x: 1550, y: 350, width: 180, height: 30}
+            {x: 0, y: 430, width: 170, height: 30},
+            {x: 220, y: 380, width: 115, height: 30},
+            {x: 385, y: 320, width: 115, height: 30},
+            {x: 550, y: 370, width: 115, height: 30},
+            {x: 715, y: 300, width: 115, height: 30},
+            {x: 880, y: 350, width: 115, height: 30},
+            {x: 1045, y: 280, width: 115, height: 30},
+            {x: 1210, y: 330, width: 115, height: 30},
+            {x: 1375, y: 290, width: 180, height: 30}
         ]
     },
 
     // LEVEL 9
     {
         platforms: [
-            {x: 0, y: 430, width: 150, height: 30},
-            {x: 220, y: 320, width: 90, height: 30},
-            {x: 390, y: 230, width: 90, height: 30},
-            {x: 560, y: 350, width: 90, height: 30},
-            {x: 730, y: 200, width: 90, height: 30},
-            {x: 900, y: 300, width: 90, height: 30},
-            {x: 1080, y: 180, width: 90, height: 30},
-            {x: 1260, y: 270, width: 90, height: 30},
-            {x: 1450, y: 190, width: 100, height: 30},
-            {x: 1680, y: 320, width: 180, height: 30}
+            {x: 0, y: 430, width: 165, height: 30},
+            {x: 215, y: 380, width: 110, height: 30},
+            {x: 375, y: 320, width: 110, height: 30},
+            {x: 535, y: 370, width: 110, height: 30},
+            {x: 695, y: 300, width: 110, height: 30},
+            {x: 855, y: 350, width: 110, height: 30},
+            {x: 1015, y: 280, width: 110, height: 30},
+            {x: 1175, y: 330, width: 110, height: 30},
+            {x: 1335, y: 270, width: 110, height: 30},
+            {x: 1495, y: 330, width: 180, height: 30}
         ]
     },
 
     // LEVEL 10 - Challenge
     {
         platforms: [
-            {x: 0, y: 430, width: 150, height: 30},
-            {x: 220, y: 340, width: 80, height: 30},
-            {x: 380, y: 230, width: 80, height: 30},
-            {x: 540, y: 320, width: 80, height: 30},
-            {x: 700, y: 190, width: 80, height: 30},
-            {x: 860, y: 280, width: 80, height: 30},
-            {x: 1020, y: 160, width: 80, height: 30},
-            {x: 1190, y: 260, width: 80, height: 30},
-            {x: 1360, y: 180, width: 80, height: 30},
-            {x: 1540, y: 270, width: 100, height: 30},
-            {x: 1770, y: 350, width: 200, height: 30}
+            {x: 0, y: 430, width: 160, height: 30},
+            {x: 210, y: 380, width: 105, height: 30},
+            {x: 365, y: 320, width: 105, height: 30},
+            {x: 520, y: 370, width: 105, height: 30},
+            {x: 675, y: 300, width: 105, height: 30},
+            {x: 830, y: 350, width: 105, height: 30},
+            {x: 985, y: 280, width: 105, height: 30},
+            {x: 1140, y: 330, width: 105, height: 30},
+            {x: 1295, y: 270, width: 105, height: 30},
+            {x: 1450, y: 320, width: 105, height: 30},
+            {x: 1605, y: 350, width: 200, height: 30}
         ]
     }
 ];
